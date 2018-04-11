4c150814976e99e9c21225031777b0e70c67b717
demariadaniel <ddemaria@brainstation.io>
2018-04-11 12:02:06 -0400

    Auto Login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import {Route, Switch} from 'react-router';
+import axios from 'axios';
+import {withRouter} from 'react-router';
+import {Link, Route, Switch} from 'react-router-dom';
 import './App.css';
 
-class App extends Component {
+class _App extends Component {
   state = {
     items : [1,2,3]
   }
@@ -13,6 +15,18 @@ class App extends Component {
       ])
     })
   }
+  componentDidMount(){
+    axios.get('http://localhost:8080/auth')
+      .then(res =>{
+        console.log(res.data)
+        if (res.data.loggedIn) {
+          this.props.history.push('/spotify')
+        } else {
+          localStorage.auth = res.data;
+          window.location = `https://accounts.spotify.com/authorize/?client_id=${res.data.id}&response_type=code&redirect_uri=http://localhost:3000/spotify`
+        }
+    })
+  }
   render() {
     let {items} = this.state
     console.log(items)
@@ -21,22 +35,32 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Spotify</h1>
         </header>
-        <div>
-          <button
-            onClick={this.add}>
-              Add an Item
-          </button>
-        </div>
+          {localStorage.auth ?
+            null :
+            <div>
+              <button className="button button-primary"
+                onClick={this.auth}>
+                Login
+              </button>
+            </div>
+          }
         <Switch>
         <Route path="/spotify"
           render={()=>(
-            <div className="items">
-              {items.map((item)=>(
-                <div className="item">
-                  {item}
-                </div>
-                )
-              )}
+            <div>
+              <button
+                className="button button-primary"
+                onClick={this.add}>
+                  Add an Item
+              </button>
+              <div className="items">
+                {items.map((item)=>(
+                  <div className="item">
+                    {item}
+                  </div>
+                  )
+                )}
+              </div>
             </div>
             )}
           />
@@ -46,4 +70,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(_App);
