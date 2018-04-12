d9909a3c15fb36f2294de252ba6d2cd087e92b8c
demariadaniel <ddemaria@brainstation.io>
2018-04-11 22:34:31 -0400

    Fix Query Route Blocking

--- api/server.js
+++ api/server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 require('dotenv').config()
 
+let loggedIn = false;
+
 app.use(bodyParser());
 
 app.use((req, res, next) => {
@@ -17,9 +19,11 @@ app.listen(8080, ()=>{
 })
 
 app.get('/auth', (req, res)=>{
-  // axios.get(`https://accounts.spotify.com/authorize/?client_id=${process.env.CLIENT_ID}&response_type=code&redirect_uri=http%3A%2F%2Flocalhost:3000%2Fspotify`)
-  //   .then(response =>{
-  //     console.log('promise')
-      res.send(process.env.CLIENT_ID)
-    // })
+  console.log(loggedIn)
+  if (loggedIn) {
+    res.send({loggedIn: true})
+  } else {
+    res.send({loggedIn: false, id: process.env.CLIENT_ID})
+    loggedIn = true;
+  }
 })
